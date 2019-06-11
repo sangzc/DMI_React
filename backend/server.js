bc43aa029e2109f0f979e995197c8c848e818215
Sandy <sangzicao@gmail.com>
2019-06-11 14:39:20 -0700

    add post route to prepend new color

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+// Set up body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const colors = ['red', 'blue', 'yellow', 'green'];
+
+// Route for getting all the colors
+app.get('/colors/', (req, res) => {
+  res.json(colors);
+});
+
+// Add a new color to the list
+app.post('/colors/', (req, res) => {
+  colors.unshift(req.body.color);
+  res.json(colors);
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
