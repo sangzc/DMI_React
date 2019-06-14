2bae4194f21c8584c1351afa42a2214c5a6aa8ad
Sandy <sangzicao@gmail.com>
2019-06-13 22:59:42 -0700

    add new color form with controlled state

--- app/containers/ColorPage/index.js
+++ app/containers/ColorPage/index.js
@@ -5,14 +5,43 @@
  *
  */
 
-import React from 'react';
+import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
-export default function HomePage() {
-  return (
-    <h1>
-      <FormattedMessage {...messages.header} />
-    </h1>
-  );
+class ColorPage extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      color: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(evt) {
+    this.setState({ [evt.target.name]: evt.target.value });
+  }
+
+  render() {
+    return (
+      <>
+        <h1>
+          <FormattedMessage {...messages.header} />
+        </h1>
+        <form>
+          <label htmlFor="color">Color:</label>
+          <input
+            id="color"
+            name="color"
+            value={this.state.color}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </>
+    );
+  }
 }
+
+export default ColorPage;
