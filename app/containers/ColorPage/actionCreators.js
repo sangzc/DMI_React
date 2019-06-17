d85a423bd25c6f2baba697710881152f6263bc0d
Sandy <sangzicao@gmail.com>
2019-06-17 03:27:54 -0700

    add colorpage action creators

---
+++ app/containers/ColorPage/actionCreators.js
@@ -0,0 +1,35 @@
+/*
+ * ColorPage Action Creators
+ *
+ */
+
+import {
+  ADD_NEW_COLOR_REQUEST,
+  ADD_NEW_COLOR_SUCCESS,
+  ADD_NEW_COLOR_ERROR,
+} from './constants';
+
+/**
+ * Sends call to API to add new color
+ *
+ * */
+export function addColorRequest(newColor) {
+  return {
+    type: ADD_NEW_COLOR_REQUEST,
+    newColor
+  };
+}
+
+export function addColorSuccess(response) {
+  return {
+    type: ADD_NEW_COLOR_SUCCESS,
+    response,
+  };
+}
+
+export function addColorError(error) {
+  return {
+    type: ADD_NEW_COLOR_ERROR,
+    error,
+  };
+}
