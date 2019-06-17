7a0b292f4753647f7d313a24e0ca0acc6637e9c9
Sandy <sangzicao@gmail.com>
2019-06-16 23:50:24 -0700

    add cors to express backend allows only the app's frontend to make requests to backend server

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const FRONTEND_BASE_URL = require('./urls');
+
 const app = express();
 const port = 5000;
+const whiteList = [FRONTEND_BASE_URL]
+
+// Set up cors options
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whiteList.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+};
+
+// Set up cors
+app.use(cors());
 
 // Set up body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// Set up cors
-// const corsOptions = {
-//   origin: function (origin, callback) {
-//     if (whitelist.indexOf(origin) !== -1) {
-//       callback(null, true)
-//     } else {
-//       callback(new Error('Not allowed by CORS'))
-//     }
-//   },
-// };
-// app.use(cors(corsOptions));
 
 const colors = ['red', 'blue', 'yellow', 'green'];
 
