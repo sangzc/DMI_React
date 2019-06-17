e394253725157bfc86e8938dcc279cbff7716f33
Sandy <sangzicao@gmail.com>
2019-06-17 03:22:25 -0700

    replace actions with action creators, add addColorSaga

--- app/containers/HomePage/sagas.js
+++ app/containers/HomePage/sagas.js
@@ -1,36 +1,39 @@
-import { call, put, all, takeEvery } from 'redux-saga/effects';
-import { GET_COLORS_REQUEST, 
-         GET_COLORS_SUCCESS, 
-         GET_COLORS_ERROR } from './constants';
+import { call, put } from 'redux-saga/effects';
+
 import { getAllColorsAPI } from './apis';
+import { addNewColorAPI } from '../ColorPage/apis';
+import { getColorsSuccess, getColorsError } from './actionCreators';
+import { addColorSuccess, addColorError } from '../ColorPage/actionCreators';
+
 
 /**
- * Saga worker that is responsible for getting all 
+ * Saga worker that is responsible for getting all
  * the list of colors from the backend
  */
 export function* getColorsSaga(action) {
   try {
     // request to call the api for all the colors
     const response = yield call(getAllColorsAPI);
-    yield put({
-      type: GET_COLORS_SUCCESS,
-      response,
-    });
-  } catch (e) {
+    yield put(getColorsSuccess(response));
+  } catch (error) {
     // catch the error
-    yield put({
-      type: GET_COLORS_ERROR,
-      error,
-    });
+    yield put(getColorsError(error));
   }
 }
 
-export function* watchColorsSaga() {
-  yield takeEvery(GET_COLORS_REQUEST, getColorsSaga)
-}
+/**
+ * Saga worker that is responsible for
+ * adding a new color to the backend
+ */
+export function* addColorSaga(action) {
 
-export function* rootSaga() {
-  yield all([
-    watchColorsSaga(),
-  ])
+  try {
+    // request to call the api for all the colors
+    const response = yield call(addNewColorAPI, action.newColor);
+    yield put(addColorSuccess(response));
+
+  } catch (error) {
+    // catch the error
+    yield put(addColorError(error));
+  }
 }
