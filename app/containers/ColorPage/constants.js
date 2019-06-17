d4b1ae25b6e6cb5e62f95b2e5cccb2977a6f028d
Sandy <sangzicao@gmail.com>
2019-06-17 03:28:42 -0700

    add constants for colorpage

---
+++ app/containers/ColorPage/constants.js
@@ -0,0 +1,3 @@
+export const ADD_NEW_COLOR_REQUEST = 'DMI_Tech_Screen/HomePage/ADD_NEW_COLOR_REQUEST';
+export const ADD_NEW_COLOR_SUCCESS = 'DMI_Tech_Screen/HomePage/ADD_NEW_COLOR_SUCCESS';
+export const ADD_NEW_COLOR_ERROR = 'DMI_Tech_Screen/HomePage/ADD_NEW_COLOR_ERROR';
