a32b47e68d1fd325704d5a78aaf847a1c3c8bd6c
Sandy <sangzicao@gmail.com>
2019-06-16 23:56:18 -0700

    rename homepage constants for more clarity

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -1,3 +1,3 @@
-export const GET_COLORS = 'DMI_Tech_Screen/HomePage/GET_COLORS';
+export const GET_COLORS_REQUEST = 'DMI_Tech_Screen/HomePage/GET_COLORS_REQUEST';
 export const GET_COLORS_SUCCESS = 'DMI_Tech_Screen/HomePage/GET_COLORS_SUCCESS';
 export const GET_COLORS_ERROR = 'DMI_Tech_Screen/HomePage/GET_COLORS_ERROR';
