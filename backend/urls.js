052777ca3fbbd33cef25d64ac1d03d0ad82f1fcd
Sandy <sangzicao@gmail.com>
2019-06-17 03:27:08 -0700

    add port as a variable to backend url for consistency

--- backend/urls.js
+++ backend/urls.js
@@ -1,7 +1,9 @@
-BACKEND_BASE_URL = 'http://localhost:5000';
-FRONTEND_BASE_URL = 'http://localhost:3000';
+const BACKEND_PORT = require('./server');
 
-module.exports = { 
+const BACKEND_BASE_URL = `http://localhost:${BACKEND_PORT}`;
+const FRONTEND_BASE_URL = 'http://localhost:3000';
+
+module.exports = {
   BACKEND_BASE_URL,
   FRONTEND_BASE_URL,
-}
+};
