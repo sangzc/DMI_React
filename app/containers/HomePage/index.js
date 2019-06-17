007e44b5b3d5c022dd10bb8d803c8cbee0375056
Sandy <sangzicao@gmail.com>
2019-06-16 23:57:42 -0700

    add api call to homepage component

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -9,16 +9,23 @@ import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import messages from './messages';
-import getColors from './actions';
+import { getColorsRequest } from './actionCreators';
 
 // eslint-disable-next-line react/prefer-stateless-function
 class HomePage extends Component {
-  async componentDidMount() {
-    this.props.getColors();
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.props.getColorsRequest();
   }
 
   render() {
-    const listOfColors = this.props.colors.map(c => <li>{c}</li>);
+    const listOfColors = this.props.colors 
+      ? this.props.colors.map(c => <li>{c}</li>)
+      : <li></li>
     return (
       <>
         <h1>
@@ -31,11 +38,11 @@ class HomePage extends Component {
 }
 
 function mapStateToProps(state) {
-  return { colors: state.home.colors };
+  return { colors: state.home };
 }
 
 const mapDispatchToProps = {
-  getColors,
+  getColorsRequest,
 };
 
 const withConnect = connect(
