b1e2ea927ff28798ab6c61abe8e0e0cfcfc57050
Sandy <sangzicao@gmail.com>
2019-06-17 17:25:11 -0700

    clean up indentation

--- app/containers/HomePage/apis.js
+++ app/containers/HomePage/apis.js
@@ -4,9 +4,6 @@ const BACKEND_BASE_URL = 'http://localhost:5000';
 
 // api call to backend to fetch all colors
 export async function getAllColorsAPI() {
-  const response = await axios.get(
-    `${BACKEND_BASE_URL}/colors`,
-  );
+  const response = await axios.get(`${BACKEND_BASE_URL}/colors`);
   return response.data;
 }
-
