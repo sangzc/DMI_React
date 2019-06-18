8b3781284ada5ac755dd237534191c5ed401fb5f
Sandy <sangzicao@gmail.com>
2019-06-17 17:57:49 -0700

    added isLoading and initialLoad to reducers

--- app/containers/HomePage/reducer.js
+++ app/containers/HomePage/reducer.js
@@ -7,19 +7,44 @@
  *
  */
 
-import { GET_COLORS_SUCCESS } from './constants';
-import { ADD_NEW_COLOR_SUCCESS } from '../ColorPage/constants';
+import {
+  GET_COLORS_SUCCESS,
+  GET_COLORS_REQUEST,
+  GET_COLORS_ERROR,
+} from './constants';
+import {
+  ADD_NEW_COLOR_SUCCESS,
+  ADD_NEW_COLOR_REQUEST,
+  ADD_NEW_COLOR_ERROR,
+} from '../ColorPage/constants';
 
 // The initial state of the App
-export const initialState = [];
+export const initialState = {
+  colors: [],
+  isLoading: true,
+  initialLoad: true,
+};
 
 /* eslint-disable default-case, no-param-reassign */
 export default function homeReducer(state = initialState, action = {}) {
   switch (action.type) {
+    case GET_COLORS_REQUEST:
+      return { ...state, isLoading: true };
+
     case GET_COLORS_SUCCESS:
-      return [...action.response];
+      return { colors: [...action.response], isLoading: false, initialLoad: false };
+
+    case GET_COLORS_ERROR:
+      return { ...state, isLoading: false };
+
+    case ADD_NEW_COLOR_REQUEST:
+      return { ...state, isLoading: true };
+
     case ADD_NEW_COLOR_SUCCESS:
-      return [...action.response];
+      return { ...state, colors: [...action.response], isLoading: false };
+
+    case ADD_NEW_COLOR_ERROR:
+      return { ...state, isLoading: false };
 
     default:
       return state;
